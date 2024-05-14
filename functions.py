6b40f36b64afb6a7a518a56ae76c0a5a9a29e591
unknown <decarch19@gmail.com>
2024-05-14 14:23:52 +0900

    便利関数

--- functions.py
+++ functions.py
@@ -1,7 +1,13 @@
 from sklearn.tree import export_graphviz as EG
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_log_error
 from pydotplus import graph_from_dot_data as GFDD
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as stats
 
 def depth(jsn, var=""):
     if isinstance(jsn, dict):
@@ -39,6 +45,66 @@ def effecttest(df, columns, y_name):
         lab.append(tmp_lab)
     return ave, dtr, pos, lab
 
+
+
+def effecttest2(df, columns, y_name, auto=False):
+    ave = []
+    dtr = []
+    pos = []
+    lab = []
+    x = 0
+    for col in columns:
+        values = list(set(df[col].values))
+        tmp_ave = []
+        tmp_dtr = []
+        tmp_pos = []
+        tmp_lab = []
+        for val in values:
+            df_tmp = df[df[col]==val]
+            tmp_ave.append(df_tmp[y_name].mean())
+            tmp_dtr.append(df_tmp[y_name])
+            tmp_pos.append(x)
+            tmp_lab.append(col+"_"+str(val))
+            x = x + 1
+        ave.append(tmp_ave)
+        dtr.append(tmp_dtr)
+        pos.append(tmp_pos)
+        lab.append(tmp_lab)
+    if auto:
+        for i in range(len(dtr)):
+            for j in range(len(dtr[i])):
+                for k in range(j, len(dtr[i])):
+                    if j != k:
+                        f, p = stats.bartlett(dtr[i][0], dtr[i][1])
+                        if (2 * p) <= 0.05:
+                            t, p = stats.ttest_ind(dtr[i][j], dtr[i][k], equal_var=False)
+                        else:
+                            t, p = stats.ttest_ind(dtr[i][j], dtr[i][k], equal_var=True)
+                        print(lab[i][j], lab[i][k])
+                        print("t   = %f"%(t))
+                        print("p   = %f"%(p))
+                        print("val = %f"%(ave[i][j]-ave[i][k]))
+                        print()
+        for i in range(len(dtr)):
+            plt.boxplot(dtr[i], positions=pos[i], labels=lab[i])
+            plt.plot(pos[i], ave[i], marker="x")
+        plt.xticks(rotation=90)
+        plt.show()
+    return ave, dtr, pos , lab
+
+
+def corr_sort(df):
+    corr = df.corr().values
+    dst = []
+    for i in range(0, len(df.corr().columns)):
+        for j in range(0, i):
+            if i != j:
+                dst.append([df.corr().columns[i], df.corr().columns[j], corr[i][j]])
+    df_corr = pd.DataFrame(dst)
+    df_corr.columns = ["因子1", "因子2", "相関係数"]
+    return df_corr.sort_values("相関係数", ascending=False)
+
+
 def pcorr(df):
     pcor = []
     corr = df.corr()
@@ -71,6 +137,22 @@ def miss_pred(y_test, y_pred, model):
     df_miss.columns = ["ID", "real", "predict", "probably"]
     return df_miss
 
+def adjr2_score(y_test, y_pred, x):
+    return 1 - (1 - r2_score(y_test, y_pred)) * (len(y_test) - 1) / (len(y_test) - len(x.columns) - 1)
+
+def regression_report(y_test, y_pred, x):
+    print("R2      : %f"%(r2_score(y_test, y_pred)))
+    print("adjR2   : %f"%(adjr2_score(y_test, y_pred, x)))
+    print("MSE     : %f"%(mean_squared_error(y_test, y_pred)))
+    print("RMSE    : %f"%(np.sqrt(mean_squared_error(y_test, y_pred))))
+    try:
+        print("RMSLE   : %f"%(np.sqrt(mean_squared_log_error(y_test, y_pred, squared=False))))
+    except:
+        _ = 0
+    print("MAE     : %f"%(mean_absolute_error(y_test, y_pred)))
+    pass
+
+
 def factor_exp(pca, x):
     col = []
     for i in range(len(x.columns)):
@@ -101,3 +183,44 @@ def x_importance(x, y, dtc):
     graph = GFDD(dotdata)
     graph.write_png("CART.png")
     return df_imp
+
+def showline_clf(x, y, model, modelname, x0="x0", x1="x1"):
+    fig, ax = plt.subplots(figsize=(8, 6))
+    X, Y = np.meshgrid(np.linspace(*ax.get_xlim(), 1000), np.linspace(*ax.get_ylim(), 1000))
+    XY = np.column_stack([X.ravel(), Y.ravel()])
+    x = preprocessing.minmax_scale(x)
+    model.fit(x, y)
+    Z = model.predict(XY).reshape(X.shape)
+    plt.contourf(X, Y, Z, alpha=0.1, cmap="brg")
+    plt.scatter(x[:, 0], x[:, 1], c=y, cmap="brg")
+    plt.xlim(min(x[:, 0]), max(x[:, 0]))
+    plt.ylim(min(x[:, 1]), max(x[:, 1]))
+    plt.title(modelname)
+    plt.colorbar()
+    plt.xlabel(x0)
+    plt.ylabel(x1)
+    plt.show()
+
+def ci_plot(df, per=0.95):
+    x = 0
+    for col in df.columns:
+        var = df[col].var()
+        mean = df[col].mean()
+        trange = stats.t(loc=mean, scale=np.sqrt(var/len(df[col].values)), df=len(df[col].values)-1)
+        low, high = trange.interval(per)
+        print(col)
+        print("%f <= z <= %f"%(low, high))
+        plt.plot([low, high], [x, x], marker="|", color="#000000")
+        plt.scatter(mean, x, marker="x", color="#000000")
+        x = x + 1
+    plt.yticks(np.arange(len(df.columns)), df.columns)
+    plt.show()
+
+def boxplot(df):
+    x = 0
+    for i in range(len(df.columns)):
+        plt.boxplot(df[df.columns[i]], positions=[x], labels=[df.columns[i]])
+        plt.scatter(x, df[df.columns[i]].mean(), marker="x", color="#000000")
+        x = x + 1
+    plt.xticks(rotation=90)
+    plt.show()
